2c6531f7d2a77d36e65110dfe3693db23c03e587
kimgura <oli999@naver.com>
2024-06-27 12:04:44 +0900

    Step06_Final 에 로그인 성공후 원래 가려던 목적지로 가도록 기능 수정함

--- Step06_Final/src/main/java/test/filter/LoginFilter.java
+++ Step06_Final/src/main/java/test/filter/LoginFilter.java
@@ -1,6 +1,7 @@
 package test.filter;
 
 import java.io.IOException;
+import java.net.URLEncoder;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -41,12 +42,31 @@ public class LoginFilter implements Filter{
 		//session 영역에 id 라는 키값으로 저장된 값이 있는지 확인한다(로그인된 사용자인지 확인)
 		String id=(String)session.getAttribute("id");
 		//만일 없다면(로그인된 사용자가 아니라면)
-		if(id == null){		
+		if(id == null){	
+			/*
+			 *  로그인 페이지로 강제 리다일렉트 됬다면 
+			 *  로그인 성공후에 원래 가려던 목적지로 다시 보내야 하고
+			 *  GET 방식 전송 파라미터가 있다면 파라미터 정보도 같이 가지고 갈수 있도록 해야한다.
+			 */
+			//원래 가려던 url 정보 읽어오기
+			String url=req.getRequestURI();
+			//GET 방식 전송 파라미터를 query 문자열로 읽어오기 ( a=xxx&b=xxx&c=xxx )
+			String query=req.getQueryString();
+			//특수 문자는 인코딩을 해야한다.
+			String encodedUrl=null;
+			if(query==null) {//전송 파라미터가 없다면 
+				encodedUrl=URLEncoder.encode(url);
+			}else {
+				// 원래 목적지가 /test/xxx.jsp 라고 가정하면 아래와 같은 형식의 문자열을 만든다.
+				// "/test/xxx.jsp?a=xxx&b=xxx ..."
+				encodedUrl=URLEncoder.encode(url+"?"+query);
+			}
+			
 			//로그인 페이지로 리다일렉트 시키기
 			String cPath=req.getContextPath();
 			// ServletResponse type 을 HttpServletResponse type 으로 casting
 			HttpServletResponse res=(HttpServletResponse)response;
-			res.sendRedirect(cPath+"/user/loginform.jsp");
+			res.sendRedirect(cPath+"/user/loginform.jsp?url="+encodedUrl);
 		}else {//로그인을 한 사용자라면 
 			//관여하지 않고 요청의 흐름을 이어간다
 			chain.doFilter(request, response);
